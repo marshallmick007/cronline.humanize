a3a71c661741f9e3b8bce19bb93f71b9fd2b6d74
Jean-Francois Rioux <jeff@mantor.org>
2017-02-11 21:40:22 -0500

    Simple expression and time

--- cronline.js
+++ cronline.js
@@ -1,4 +1,8 @@
+var _ = require('lodash');
+
 module.exports = {
+  time: null,
+
   humanize: function(cronline){
     if(cronline == '* * * * *'){
       return 'at every minute';
@@ -10,30 +14,74 @@ module.exports = {
       if(fields.length != 5){
         return 'not a valid cron expression';
       } else {
-        parts = {};
+        var parsed_field = [];
 
-        parts.minute = this.parseField('minute', fields[0]);
-        parts.hour = (fields[1] == '*') ? '' : this.parseField('hour', fields[1]);
-        parts.day = (fields[2] == '*') ? '' : this.parseField('day', fields[2]);
-        parts.month = (fields[3] == '*') ? ' of every month ' : this.parseField('month', fields[3]);
-        parts.weekday = (fields[4] == '*') ? '' : this.parseField('weekday', fields[4]);
+        parsed_field.push(['minute', this.parseField(fields[0])]);
+        parsed_field.push(['hour', this.parseField(fields[1])]);
+        parsed_field.push(['day', this.parseField(fields[2])]);
+        parsed_field.push(['month', this.parseField(fields[3])]);
+        parsed_field.push(['weekday', this.parseField(fields[4])]);
 
-        // if(typeof parseInt(parts.minute) == 'number' && typeof parts.hour == 'number'){
-        minutes = "" + parts.minute;
-        string = 'At ' + fields[1] + ':' + "00".substring(0, 2 - minutes.length) + minutes;
-        // }
+        var defined_fields = _.filter(parsed_field, function (o) {
+          return o[1] !== null;
+        });
 
-        return string + parts.day + parts.month + parts.weekday;
+        if(defined_fields.length == 1)
+        {
+          o = defined_fields[0];
+          return this.simple(o);
+        }
+        else
+        {
+          var combination = {};
+          for(var i = 0; i < defined_fields.length; i++){
+            o = defined_fields[i];
+            combination[o[0]] = o[1];
+          }
+          this.parseTime(combination);
+          return 'at ' + this.time;
+        }
       }
     }
   },
-  parseField: function(name, value){
+  simple: function(value){
+    var out = '';
+    switch(value[0]){
+      case 'minute':
+        out = 'on minute ' + value[1];
+        break;
+      case 'hour':
+         out = 'at every minute of hour ' + value[1];
+         break;
+      case 'day':
+        out = 'at every minute on day of the month ' + value[1];
+        break;
+      case 'month':
+        out = 'at every minute in ' + this.monthName(o[1]);
+        break;
+      case 'weekday':
+        out = 'at every minute on ' + this.dayName(o[1]);
+        break;
+      default:
+        break;
+    }
+
+    return out;
+  },
+
+
+  parseTime: function(combination){
+    if(_.has(combination, 'minute') && _.has(combination, 'hour'))
+    {
+      this.time = combination.hour + ':' + "00".substring(0, 2 - combination.minute.length) + combination.minute;
+    }
+  },
+  parseField: function(value){
     var output = '';
 
     // *
     if(value == '*' || value.match(/^\*\/1$/)){
-      output = 'every ' + name;
-      return output;
+      return null;
     }
     // 15 OR 1,2,3,4,5
     if(value.match(/^[0-9]{1,2}$/) || value.match(/^[0-9,]+[0-9]$/)){
@@ -41,7 +89,7 @@ module.exports = {
     }
     // */5
     if(value.match(/^\*\/[0-9]{1,2}$/)){
-      output = 'every ' + value.replace(/\*\//, '') + ' ' + name;
+      output = 'every ' + value.replace(/\*\//, '');
     }
     // 5-25
     if(value.match(/^[0-9]{1,2}-[0-9]{1,2}$/)){
@@ -51,26 +99,12 @@ module.exports = {
     // 5-25/5
     if(value.match(/^[0-9]{1,2}-[0-9]{1,2}\/[0-9]{1,2}$/)){
       var range = value.match(/^([0-9]{1,2})-([0-9]{1,2})\/([0-9]{1,2})$/);
-      output = 'every ' + range[3] + ' ' + name + ' from ' + range[1] + ' to ' + range[2];
+      output = range[3] + ' from ' + range[1] + ' to ' + range[2];
     }
 
-    return this.decorate(name, output);
+    return output;
   },
-  decorate: function(name, parsed_value){
-    switch (name){
-      case 'minute':
-        return parsed_value;
-      case 'hour':
-        return parsed_value;
-      case 'day':
-        return ' on day-of-month ' + parsed_value;
-      case 'month':
-        return ' in ' + this.monthNames(parsed_value);
-      case 'weekday':
-        return ' and on ' + this.dayNames(parsed_value);
-    }
-  },
-  monthNames: function(value){
+  monthName: function(value){
 // TODO Missing 1-3 -> range ----------
     var monthNames = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var months = value.split(',');
@@ -81,7 +115,7 @@ module.exports = {
 
     return names.join(', ');
   },
-  dayNames: function(value){
+  dayName: function(value){
     var dayNames = ["", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
     var days = value.split(',');
     var names = [];
--- tests/cronline_test.js
+++ tests/cronline_test.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect;
 var Cronline = require('../cronline');
 
-describe('simple cron parsing', function(){
+describe('simple expression parsing', function(){
   it("should return an error on ivalid cron expression", function(){
     var result = Cronline.humanize("Failure, when your best isn't just good enough");
     expect(result).to.equal('not a valid cron expression');
@@ -12,8 +12,39 @@ describe('simple cron parsing', function(){
     expect(result).to.equal('at every minute');
   });
 
-  it("should parse minute", function(){
-    var result = Cronline.humanize('1 * * * *');
-    expect(result).to.equal('at minute 1');
+  describe('single values', function(){
+    it("should parse minute 1", function(){
+      var result = Cronline.humanize('1 * * * *');
+      expect(result).to.equal('on minute 1');
+    });
+
+    it("should parse hour 1", function(){
+      var result = Cronline.humanize('* 1 * * *');
+      expect(result).to.equal('at every minute of hour 1');
+    });
+
+    it("should parse day 1", function(){
+      var result = Cronline.humanize('* * 1 * *');
+      expect(result).to.equal('at every minute on day of the month 1');
+    });
+
+    it("should parse month 1", function(){
+      var result = Cronline.humanize('* * * 1 *');
+      expect(result).to.equal('at every minute in January');
+    });
+
+    it("should parse weekday 1", function(){
+      var result = Cronline.humanize('* * * * 1');
+      expect(result).to.equal('at every minute on Monday');
+    });
+  });
+  describe('single values combinations', function() {
+    it("should parse minute and hour combination", function () {
+      var result = Cronline.humanize('1 5 * * *');
+      expect(result).to.equal('at 5:01');
+
+      result = Cronline.humanize('15 15 * * *');
+      expect(result).to.equal('at 15:15');
+    });
   });
 });
